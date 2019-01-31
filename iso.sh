d2572498dc96fa76f30b4d49be6a70a90165f4f4
Andrew Steinborn <git@steinborn.me>
2019-01-30 19:16:54 -0500

    Finish rename to Goldrush

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/goldrush isodir/boot/goldrush
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
+menuentry "Goldrush" {
+	multiboot /boot/goldrush
 }
 EOF
 grub-mkrescue -o myos.iso isodir
