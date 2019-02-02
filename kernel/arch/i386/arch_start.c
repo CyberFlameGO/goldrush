8e14bfe97c27bc0101a8bb0751b058b2b6c5d7fc
Andrew Steinborn <git@steinborn.me>
2019-02-02 02:33:00 -0500

    It's having problems reading the multiboot memory map, but it's progress

--- kernel/arch/i386/arch_start.c
+++ kernel/arch/i386/arch_start.c
@@ -21,6 +21,9 @@
 
 static multiboot_info_t *multiboot_info;
 
+// kernel_arch_early_start is a special entry point, called before kernel_arch_start.
+// It is responsible for early initialization and is subject to a bare C environment.
+// Only the boot stack is available.
 void kernel_arch_early_start(uint32_t magic, uint32_t multiboot_info_ptr)
 {
     // Initialize early print support.
@@ -41,6 +44,27 @@ void kernel_arch_start()
     printf("Hello, world!\n");
 
     printf("xkcd says that a good random number is %d\n", 4);
+
+    // hmm, what can we gleam from our multiboot?
+    if (multiboot_info->flags & MULTIBOOT_INFO_MEMORY) {
+        printf("basic memory information: low: %dKiB, high: %dKiB\n", multiboot_info->mem_lower, multiboot_info->mem_upper);
+    }
+
+    if (multiboot_info->flags & MULTIBOOT_INFO_MEM_MAP) {
+        printf("we have full memory mappings!\n");
+        printf("the mappings are %d bytes long\n", multiboot_info->mmap_length);
+        multiboot_memory_map_t *mmap = multiboot_info->mmap_addr;
+        unsigned int available_pages;
+        while (mmap < multiboot_info->mmap_addr + multiboot_info->mmap_length) {
+            printf("- at 0x%x%x: 0x%x%x bytes (type %d)\n", mmap->addr_low, mmap->addr_high, mmap->len_low, mmap->len_high, mmap->type);
+            if (mmap->type == MULTIBOOT_MEMORY_AVAILABLE) {
+                available_pages += mmap->len_low >> 12;
+            }
+            printf("  skip %d\n", mmap->size);
+            mmap = (multiboot_memory_map_t*) ((unsigned long) mmap + mmap->size + sizeof(mmap->size));
+        }
+        printf("We have %d pages to work with\n", available_pages);
+    }
 
     // Run kernel_main().
     kernel_main();
